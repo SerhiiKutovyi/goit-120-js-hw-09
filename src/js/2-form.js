925a204edf5f9a7c7ccbad8ac3963db2a55ab689
SerhiiKutovyi <wagenrex@gmail.com>
2025-09-09 21:50:06 +0300

    markup gallery

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,2 +1,16 @@
-const data = Date.now();
-console.log(data);
+const formData = {
+  email: '',
+  message: '',
+};
+
+const feedbackFormRef = document.querySelector('.feedback-form');
+console.log(feedbackFormRef);
+
+feedbackFormRef.addEventListener('submit', e => {
+  e.preventDefault();
+
+  formData.email = e.target.elements.email.value;
+  formData.email = e.target.elements.message.value;
+});
+
+console.log(formData);
