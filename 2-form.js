d6d49b7ede3b9610fdd4b18fcc71be4af7fe0ef9
SerhiiKutovyi <109694632+SerhiiKutovyi@users.noreply.github.com>
2025-09-09 18:52:55 +0000

    Deploying to gh-pages from @ SerhiiKutovyi/goit-120-js-hw-09@925a204edf5f9a7c7ccbad8ac3963db2a55ab689 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                     */const o=Date.now();console.log(o);
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                     */const t={email:"",message:""},a=document.querySelector(".feedback-form");console.log(a);a.addEventListener("submit",e=>{e.preventDefault(),t.email=e.target.elements.email.value,t.email=e.target.elements.message.value});console.log(t);
 //# sourceMappingURL=2-form.js.map
